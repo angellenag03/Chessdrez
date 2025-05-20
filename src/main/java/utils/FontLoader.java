a5237c1dd0c15a436005880404e0887a52e9a0ee
angel lenag <angel.lenag03@gmail.com>
2025-05-19 22:58:10 -0700

    clase tuneada

--- src/main/java/utils/FontLoader.java
+++ src/main/java/utils/FontLoader.java
@@ -11,19 +11,20 @@ import java.io.InputStream;
  * @author Ricardo
  */
 public class FontLoader {
-    public static Font loadFont(String path, float size) throws IOException {
+    
+    public static Font loadFont(float size) throws IOException {
         try {
             // Cargar la fuente como un recurso
-            InputStream is = FontLoader.class.getResourceAsStream(path);
+            InputStream is = FontLoader.class.getResourceAsStream("Alegard.ttf");
             if (is == null) {
-                System.err.println("No se pudo cargar la fuente: " + path);
-                return new Font("Arial", Font.PLAIN, (int)size);
+                System.err.println("No se pudo cargar la fuente: " + "Alegard.ttf");
+                return new Font("Century Gothic", Font.PLAIN, (int)size);
             }
             Font font = Font.createFont(Font.TRUETYPE_FONT, is);
             return font.deriveFont(size);
         } catch (FontFormatException | IOException e) {
             e.printStackTrace();
-            return new Font("Arial", Font.PLAIN, (int)size);
+            return new Font("Century Gothic", Font.PLAIN, (int)size);
         }
     }
 }
